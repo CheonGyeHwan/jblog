1a799be98b64c3301c1e9e121e5d8d797da6d66a
CheonGyeHwan <cgh5531@naver.com>
2022-02-07 09:42:54 +0900

    jblog03 #2 - blog Main 화면 url 처리

--- jblog03/src/main/java/com/poscoict/jblog/controller/BlogController.java
+++ jblog03/src/main/java/com/poscoict/jblog/controller/BlogController.java
@@ -1,5 +1,6 @@
 package com.poscoict.jblog.controller;
 
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +12,7 @@ import com.poscoict.jblog.service.CategoryService;
 import com.poscoict.jblog.service.PostService;
 
 @Controller
+@RequestMapping("/{id:(?!assets).*}")
 public class BlogController {
 	@Autowired
 	private BlogService blogService;
@@ -21,8 +23,23 @@ public class BlogController {
 	@Autowired
 	private PostService postService;
 
-	@RequestMapping("/{id}")
-	public String main(@PathVariable("id") String id, Model model) {
+	@RequestMapping({"", "/{pathNo1}", "/pathNo1/pathNo2"})
+	public String main(@PathVariable("id") String id,
+					   @PathVariable("pathNo1") Optional<Long> pathNo1,
+					   @PathVariable("pathNo2") Optional<Long> pathNo2,
+					   Model model) {
+		// Map으로 저장해서 select 한번에 처리(Service에서) -- 수정
+		// Optional --> PathVariable은 데이터 없으면 Null이 아니라 에러 발생 => Optional 사용
+		Long categoryNo = 0L;
+		Long postNo = 0L;
+		
+		if (pathNo2.isPresent()) {
+			categoryNo = pathNo1.get();
+			postNo = pathNo2.get();
+		} else if (pathNo1.isPresent()) {
+			categoryNo = pathNo1.get();
+		}
+		
 		model.addAttribute("blog", blogService.getBlog(id));
 		model.addAttribute("category", categoryService.getCategory(id));
 		model.addAttribute("post", postService.getPost(id));
@@ -32,7 +49,7 @@ public class BlogController {
 	}
 	
 	@AdminAuth
-	@RequestMapping("/{id}/admin/basic")
+	@RequestMapping("/admin/basic")
 	public String adminBasic(@PathVariable("id") String id) {
 		return "blog/blog-admin-basic";
 	}
