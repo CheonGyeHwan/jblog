8da0b06daa776b1f0425da873d3063dba06aa1fe
CheonGyeHwan <cgh5531@naver.com>
2022-02-11 20:58:25 +0900

    jblog03, 04 #GlobalException 기능 추가

---
+++ jblog03/src/main/java/com/poscoict/jblog/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.poscoict.jblog.exception;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+	
+	@ExceptionHandler(Exception.class)
+	public String ExceptionHandler(Exception e, Model model) {
+		StringWriter errors = new StringWriter();
+		e.printStackTrace(new PrintWriter(errors));
+		
+		model.addAttribute("exception", errors.toString());
+		return "error/exception";
+	}
+	
+}
